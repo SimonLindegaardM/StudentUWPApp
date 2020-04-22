5aaf1c5c1dba6fb891fd2630b11cead2313464b2
SimonLindegaardM <62882405+SimonLindegaardM@users.noreply.github.com>
2020-04-22 11:00:53 +0200

    Added Student class
    added student class med interface

---
+++ StudentUWPApp/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace StudentUWPApp
+{
+    public class Student : INotifyPropertyChanged
+    {
+        private string _navn;
+        public Student(string Navn)
+        {
+            _navn = Navn;
+        }
+        public string Navn
+        {
+            get { return _navn; }
+            set
+            {
+                _navn = value;
+                this.OnPropertyChanged();
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null) =>
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
